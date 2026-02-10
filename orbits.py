7302853f6abe5e71a009fa733407135944006381
Joseph-Thaggard <81182140+Joseph-Thaggard@users.noreply.github.com>
2026-02-10 16:54:30 -0600

    Adding pseudocode force calculator and some thoughts on a gravitational potential map

--- orbits.py
+++ orbits.py
@@ -12,6 +12,7 @@
 
 dt = 1
 csv_path = "~/orbit_characteristics.csv"
+G = 6.6743*10^-11
 
 # Given data: Planet,  AU, eccentricity, i to eccliptic, Ω deg, ω deg,  θdeg 
 
@@ -44,3 +45,29 @@ sol = solve_ivp(rhs, [0, 2*T], np.array(r0 + v0))
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot3D(sol.y[0,:], sol.y[1,:], sol.y[2.:])
+
+# Need to add if nbody = true or nbody = false at the end
+# Consider reformating .csv to JSON
+# Presume we store values like:
+# [mass],[position],[velocty],[acceleration],nbody
+# I'm a little dumb and forgot how to do python nested vectors
+
+# Force calculator for nbody-enable objects (spacecraft), option 1
+for object in sim:
+    # Checking for nbody simulation check
+    if sim[object][end] = True:
+        for object2 in sim:
+            # Get position
+            target = object
+            pos1 = sim[target][1]
+            pos2 = sim[object2][1]
+            mass1 = sim[target][0]
+            mass2 = sim[object2][0]
+            dpos = pos2-pos1
+            force = G*(mass2*mass1)/(dpos^2)
+
+# Gravitational potential calculator for all objects: 
+# Will be calculated at each timestep for all objects
+# Calculated at each cell given the current position
+# Equation of gravitation potential: -GM/r
+# Each object adds to the potential map
